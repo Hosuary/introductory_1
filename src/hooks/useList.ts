412e807f61e2f4a2303bf989b2184da3c2ff2615
Alexey Chernousov <achernousov4@gmail.com>
2022-06-22 09:50:28 +0300

    changed the comment message in useList

--- src/hooks/useList.ts
+++ src/hooks/useList.ts
@@ -3,8 +3,10 @@ import generator from "../methods/generator";
 
 
 const useList = (length: number) => {
-  // We can expand logic of this component: add fetches, loading, etc.
-  // We don"t need use the setter of state in our task
+  /*
+   We can expand logic of this component: add fetches, loading, etc.
+   We don't need to use the setter of state in our task
+  */
 
   const [list] = useState(generator(length));
 
